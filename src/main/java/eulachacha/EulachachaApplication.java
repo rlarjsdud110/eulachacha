cb80faaadc1c9a7254618b9f1e746db973f69bc4
김건영 <rlarjsdud110@naver.com>
2024-03-17 00:57:00 +0900

    개발 세팅

---
+++ src/main/java/eulachacha/EulachachaApplication.java
@@ -0,0 +1,13 @@
+package eulachacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EulachachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EulachachaApplication.class, args);
+    }
+
+}
